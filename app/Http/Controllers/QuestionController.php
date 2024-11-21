b1ffb59868f7a8e6109887b6b2752d7fd1ca1690
Yuki1511 <t.a.ug.b.a.6789c@gmail.com>
2024-11-21 15:31:12 +0900

    ルートの修正

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
 
 class QuestionController extends Controller
 {
-    //
+    $questions = Questions::all();
+    return view('index',compact('question'));
 }
