5da66428789347a8b9c9c7183aec2d1e600fb313
Yuki1511 <t.a.ug.b.a.6789c@gmail.com>
2025-01-09 16:02:55 +0900

    ヘッダーをindexに統一

--- resources/views/question/questions_list.blade.php
+++ resources/views/question/questions_list.blade.php
@@ -5,10 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
     <title>Quenstion</title>
 </head>
 
 <body>
+    <header class="wrapper">
+        <div class="site_top">
+            <h2>Java SE11 Bronze 資格対策</h2>
+            <p>ログイン</p>
+        </div>
+        <ul class="main_nav ">
+            <a href="#">
+                <li>学習開始</li>
+            </a>
+            <a href="#">
+                <li>章選択</li>
+            </a>
+            <a href="#">
+                <li>試験概要</li>
+            </a>
+        </ul>
+    </header>
     <h2>問題一覧</h2>
     <table>
         <thead>
