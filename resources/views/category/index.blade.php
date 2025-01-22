9d5abdd26a3bac4ecad597d7e234a7ad147f8857
Yuki1511 <t.a.ug.b.a.6789c@gmail.com>
2025-01-22 15:29:03 +0900

    viewの追加（カテゴリ）

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <h1>章選択</h1>
+    <div class="category_list">
+        <ul>
+            <li><a href="#">1&nbsp;Java言語のプログラムの流れ</a></li>
+            <li><a href="#">2&nbsp;データ宣言と使用</a></li>
+            <li><a href="#">3&nbsp;演算子と分岐文</a></li>
+            <li><a href="#">4&nbsp;ループ文</a></li>
+            <li><a href="#">5&nbsp;オブジェクト指向コンセプト</a></li>
+            <li><a href="#">6&nbsp;クラス指定とオブジェクト生成、使用</a></li>
+            <li><a href="#">7&nbsp;継承とポリモーフィズム</a></li>
+        </ul>
+    </div>
+
+</x-app-layout>
